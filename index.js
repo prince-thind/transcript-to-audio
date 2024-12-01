091503579cca8bf68b371fc472eec73b851ab8a8
Prince Thind <princethind799@gmail.com>
2024-12-01 16:19:49 +0530

    add mergeaudioscript

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const convertToCSV = require("./lib/convertToCSV");
 const path = require("path");
+const processCSV = require("./lib/csvToAudio");
 
 const inputFilePath = path.join(__dirname, "./input/transcript.txt");
 const outputFilePath = path.join(__dirname, "./output/output.csv");
 
 async function main() {
-  //   await convertToCSV(inputFilePath, outputFilePath);
+  //do cleanup of temp
+  //await convertToCSV(inputFilePath, outputFilePath);
+  // await processCSV();
 }
 
 main();
