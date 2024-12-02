8f355f32a446b0b8488cb8c45521a364f9f021fe
Prince Thind <princethind799@gmail.com>
2024-12-02 11:10:55 +0530

    add direct video subtitles feature

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const path = require("path");
 const processCSV = require("./lib/csvToAudio");
 const cleanup = require("./lib/cleanup");
 const mergeMP3Files = require("./lib/mergeAudio");
+const convertCsvToSrt = require("./lib/convertToSRT");
+const embedSubtitles = require("./lib/mergeSRTVIdeo");
+const replaceAudioInVideo = require("./lib/replaceVideoSound");
 
 const inputFilePath = path.join(__dirname, "./input/transcript.txt");
 const outputFilePath = path.join(__dirname, "./output/output.csv");
@@ -12,6 +15,9 @@ async function main() {
   await convertToCSV(inputFilePath, outputFilePath);
   await processCSV();
   await mergeMP3Files();
+  await replaceAudioInVideo();
+  await convertCsvToSrt();
+  await embedSubtitles();
 }
 
 main();
